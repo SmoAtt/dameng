5796476381d4d3cbb4d4f05a41af06f082b57dfd
弈起 <1342456515@qq.com>
2023-06-29 20:00:32 +0800

    第一次添加

---
+++ src/main/java/com/cuc/ctrip/api/form/HelloSayGoodByeForm.java
@@ -0,0 +1,12 @@
+package com.cuc.ctrip.api.form;
+
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+
+@ApiModel
+@Data
+public class HelloSayGoodByeForm {
+    private String name;
+}
