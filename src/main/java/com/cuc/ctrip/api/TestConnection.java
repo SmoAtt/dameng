ae5318bae4af7027ca7f33b85c7e92706342366f
弈起 <1342456515@qq.com>
2023-06-29 20:16:47 +0800

    第一次添加

--- src/main/java/com/cuc/ctrip/api/TestConnection.java
+++
@@ -1,9 +0,0 @@
-package com.cuc.ctrip.api;
-
-import com.cuc.ctrip.api.config.SQL;
-
-public class TestConnection {
-    public static void main(String[] args) {
-        SQL.GetResults("select Top 3 * from Sales.Orders");
-    }
-}
